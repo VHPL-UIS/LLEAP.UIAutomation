12aa3bc6abe1e6614454d756ef7d5c19a1a7a830
VHPL-UIS <heidaripour.l.vahid@uis.no>
2026-07-25 12:35:56 +0200

    Page object for the simulation home page

---
+++ LLEAP.UITests/Pages/SimulationHomePage.cs
@@ -0,0 +1,17 @@
+﻿using FlaUI.Core.AutomationElements;
+
+namespace LLEAP.UITests.Pages;
+
+public class SimulationHomePage : BasePage
+{
+    public SimulationHomePage(Window rootWindow) : base(rootWindow)
+    { }
+    public void OpenInstructorApplication()
+        => ClickBy(SimulationHomeLocators.InstructorApplicationTile);
+
+    public void CollectClientLogs()
+    {
+        WaitFor(SimulationHomeLocators.HelpTile).RightClick();
+        ClickBy(SimulationHomeLocators.CollectClientLogsMenuItem);
+    }
+}
